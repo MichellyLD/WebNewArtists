f7fd68922ed836a1aaab02708cfd7f65fd5c759e
Michelly <114167405+MichellyLD@users.noreply.github.com>
2023-12-06 22:42:21 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+declare (strict_types = 1);
+
+session_start();
+
+use Newartists\Core\Router;
+
+require __DIR__ . "/vendor/autoload.php";
+require __DIR__. "/app/config.php";
+require __DIR__. "/app/rotas.php";
+require __DIR__. "/app/Core/helper.php";
+
+
+
+$url = $_GET['url'] ?? "";
+unset($_GET['url']);
+$metodoHttp = $_SERVER["REQUEST_METHOD"];
+
+Router::exec($url,$metodoHttp);
+
+
